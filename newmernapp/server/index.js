1fa26e87b639e8edf908cd87d67653a53f617d35
andy <andyugbawa@gmail.com>
2026-05-13 09:11:09 +0100

    changes cors credentials

--- newmernapp/server/index.js
+++ newmernapp/server/index.js
@@ -11,10 +11,17 @@ const app = express()
 
 
 app.use(express.json())
-app.use(cors({
-  origin: "http://localhost:5173",
+
+ app.use(cors({
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }))
+
+
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }))
 app.use(cookieParser())
 
 // mongoose.connect("mongodb://localhost:27017/employee")
