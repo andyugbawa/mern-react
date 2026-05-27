fe36f3ce6b1d7c3cf2ce1d31b4abe6ad17aa9291
andy <andyugbawa@gmail.com>
2026-05-27 16:39:14 +0100

    images added to public folder

--- newmernapp/client/src/Home.jsx
+++ newmernapp/client/src/Home.jsx
@@ -2,7 +2,18 @@ import "./style.css"
 
 
 function Home() {
-  return <h1>Welcome Home 🎉</h1>
+  return(
+    <div>
+
+    <h1>Welcome Home</h1>
+
+    <div>
+            <img src="/action.jpeg" alt="Movie" />
+    </div>
+    </div>
+
+
+  ) 
 }
 
 export default Home
