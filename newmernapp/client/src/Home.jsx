26079b73fdb699ebec392391a9d0ff43d49b4bbc
andy <andyugbawa@gmail.com>
2026-06-24 19:34:23 +0100

    adding style on Home Page

--- newmernapp/client/src/Home.jsx
+++ newmernapp/client/src/Home.jsx
@@ -8,8 +8,42 @@ function Home() {
     <h1>Welcome Home</h1>
 
     <div>
-            <img src="/action.jpeg" alt="Movie" />
+            {/* <img src="/action.jpeg" alt="Movie" /> */}
     </div>
+   
+      <div class="parent slide-left">
+      <div class="content">
+        <div class="box1">
+        </div>
+        <div class="box2">
+        </div>
+        <div class="box3">
+        </div>
+        <div class="box4">
+        </div>
+        </div>
+        <div class="content1">
+        <div class="box5">
+        </div>
+        <div class="box6">
+        </div>
+        <div class="box7">
+        </div>
+        </div>
+      <div class="content2">
+        <div class="box1">
+        </div>
+        <div class="box2">
+        </div>
+        <div class="box3">
+        </div>
+        <div class="box4">
+        </div>
+        </div>
+      </div>
+   
+
+
     </div>
 
 
