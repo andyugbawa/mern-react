9561ead135df3d7c222849d1c49855c19c339f6c
andy <andyugbawa@gmail.com>
2026-06-25 15:40:42 +0100

    image added

--- newmernapp/client/src/Home.jsx
+++ newmernapp/client/src/Home.jsx
@@ -1,7 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
 import "./style.css"
 
 
 function Home() {
+ const [fliped, setFliped] =useState(false)
+
   return(
     <div>
 
@@ -18,15 +22,19 @@ function Home() {
 
       <div className="content">
         <div className="box1">
+        <img src="/action.jpeg" alt="Movie" />
         </div>
+
+{/* 
         <div className="box2">
         </div>
         <div className="box3">
         </div>
         <div className="box4">
+        </div> */}
+
         </div>
-        </div>
-         
+{/*          
         <div className="content1">
         <div className="box5">
         </div>
@@ -34,8 +42,9 @@ function Home() {
         </div>
         <div className="box7">
         </div>
-        </div> 
-        
+
+        </div>  */}
+{/*         
       <div className="content2">
         <div className="box1">
         </div>
@@ -45,7 +54,11 @@ function Home() {
         </div>
         <div className="box4">
         </div>
-        </div> 
+
+
+        </div>  */}
+
+        
       </div>
    
 
