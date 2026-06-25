987e3e5149190baa1341898ad82b82454ca12c9f
andy <andyugbawa@gmail.com>
2026-06-25 15:06:22 +0100

    new keyframes added

--- newmernapp/client/src/Home.jsx
+++ newmernapp/client/src/Home.jsx
@@ -28,22 +28,22 @@ function Home() {
         </div>
          
         <div className="content1">
-        <div class="box5">
+        <div className="box5">
         </div>
-        <div class="box6">
+        <div className="box6">
         </div>
-        <div class="box7">
+        <div className="box7">
         </div>
         </div> 
         
       <div className="content2">
-        <div class="box1">
+        <div className="box1">
         </div>
-        <div class="box2">
+        <div className="box2">
         </div>
-        <div class="box3">
+        <div className="box3">
         </div>
-        <div class="box4">
+        <div className="box4">
         </div>
         </div> 
       </div>
