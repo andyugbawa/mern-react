015a6b6c63eeb4d32ddf4eea540b37865e91e675
andy <andyugbawa@gmail.com>
2026-07-10 11:31:17 +0100

    Home button added in comments/request page

--- newmernapp/client/src/Comments.jsx
+++ newmernapp/client/src/Comments.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Comments() {
+
+  
+  const handleHome = () => {
+  navigate("/home");
+};
+
   return (
+    
     <div>
+       <div>
+        <button onClick={handleHome}>Home</button>
+       </div>
+
       <h1>COMMENTS</h1>
     </div>
   )
